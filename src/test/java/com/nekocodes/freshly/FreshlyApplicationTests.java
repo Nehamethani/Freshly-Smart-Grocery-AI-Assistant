06833bd477c13bc9e1d72c19f6ed993a97a45199
Neha Methani <nehamethani54@gmail.com>
2025-05-07 22:50:08 +0530

    Initial Commit

---
+++ src/test/java/com/nekocodes/freshly/FreshlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nekocodes.freshly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
