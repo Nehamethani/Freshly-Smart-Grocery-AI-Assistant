c93edfffb4c81d81a71c70fceaeef0cca6d42787
Neha Methani <nehamethani54@gmail.com>
2025-05-14 20:54:29 +0530

    Frontend form - modified UI

--- frontend/src/components/InputForm.tsx
+++ frontend/src/components/InputForm.tsx
@@ -1,12 +1,15 @@
-
+import Multiselect from "multiselect-react-dropdown";
+import Select from "react-select";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
 const InputForm = () => {
+
     return (
-        <div className='min-h-[calc(100vh-8rem)] flex flex-col items-center justify-center'>
-            <h1 className='text-3xl font-bold mb-4'>Welcome to the Input Form</h1>
-            <div className='flex items-center bg-black justify-center h-full'>
-                <div className='p-35 bg-white shadow-md rounded-sm'>
-                    <h2>Personal Information</h2>
+        <div className='min-h-[calc(100vh-8rem)] flex flex-col items-center justify-center w-full h-full aspect-[2/1]'>
+            <div className='w-full max-w-4xl flex items-center justify-center h-5/6 relative'>
+                <div className='flex-1 aspect-square w-1/2 p-6 bg-white shadow-md rounded-sm'>
+                    <h2 className="text-lg mb-4">Personal Information</h2>
                     <form>
                         <div className='mb-4'>
                             <label htmlFor="name" className='block text-gray-700'>Name:</label>
@@ -19,54 +22,119 @@ const InputForm = () => {
                         <div className='mb-4'>
                             <label htmlFor="gender" className='block text-gray-700'>Gender:</label>
                             <select name="gender" className="border border-gray-300 p-2 rounded w-full">
-                                <option value="">Male</option>
-                                <option value="None">Female</option>
-                                <option value="Vegetarian">Don't want to share</option>
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                                <option value="NA">Don't want to share</option>
+                            </select>
+                        </div>
+                        <div className='mb-4'>
+                            <label htmlFor="height" className='block text-gray-700'>Height(cm):</label>
+                            <input type="text" id="height" className='border border-gray-300 p-2 rounded w-full' />
+                        </div>
+                        <div className='mb-4'>
+                            <label htmlFor="weight" className='block text-gray-700'>Weight(kg):</label>
+                            <input type="text" id="weight" className='border border-gray-300 p-2 rounded w-full' />
+                        </div>
+                        <div className='mb-4'>
+                            <label htmlFor="activity-level" className='block text-gray-700'>Activity Level:</label>
+
+                            <select name="activity-level" className="border border-gray-300 p-2 rounded w-full">
+                                <option value="sedentary">Sedentary</option>
+                                <option value="lightly-active">Lightly Active</option>
+                                <option value="active">Active</option>
+                                <option value="very-active">Very Active</option>
                             </select>
                         </div>
 
                     </form>
                 </div>
-                <div className='p-35 bg-indigo-500 text-white shadow-md rounded-sm'>
-                    <h2>Dietary Preferences</h2>
+                <div className='flex-1 aspect-square w-1/2 p-6 bg-indigo-500 text-white shadow-md rounded-sm'>
+                      <h2 className="text-lg mb-4">Dietary Information</h2>
                     <form>
-                        <div className='mb-4'>
-                            <label htmlFor="name" className='block'>Diet Type:</label>
-                            <select name="diet" className="border bg-indigo-500 border-gray-300 p-2 rounded w-full">
-                                <option value="">Diet Type</option>
-                                <option value="None">None</option>
-                                <option value="Vegetarian">Vegetarian</option>
-                                <option value="Vegan">Vegan</option>
-                                <option value="Keto">Keto</option>
-                                <option value="Paleo">Paleo</option>
-                                <option value="Gluten-Free">Gluten-Free</option>
-                            </select>
-                        </div>
-                        <div className='mb-4'>
-                            <label htmlFor="name" className='block'>Diet Type:</label>
-                            <select name="cuisines" className="border bg-indigo-500 border-gray-300 p-2 rounded w-full">
-                                <option value="Indian">Indian</option>
-                                <option value="Italian">Italian</option>
-                                <option value="Chinese">Chinese</option>
-                                <option value="Mediterranean">Mediterranean</option>
-                                <option value="Mexican">Mexican</option>
-                            </select>
-                        </div>
-                          <div className='mb-4'>
-                            <label htmlFor="name" className='block'>Diet Type:</label>
-                            <select name="cuisines" className="border bg-indigo-500 border-gray-300 p-2 rounded w-full">
-                                <option value="Indian">Indian</option>
-                                <option value="Italian">Italian</option>
-                                <option value="Chinese">Chinese</option>
-                                <option value="Mediterranean">Mediterranean</option>
-                                <option value="Mexican">Mexican</option>
-                            </select>
-                        </div>
+                        <label className="h-25">
+                            Favorite Cuisines:
+                            <Select
+                                isMulti
+                                name="cuisines"
+                                options={[
+                                    { value: 'Indian', label: 'Indian' },
+                                    { value: 'Italian', label: 'Italian' },
+                                    { value: 'Chinese', label: 'Chinese' },
+                                    { value: 'Mediterranean', label: 'Mediterranean' },
+                                    { value: 'Mexican', label: 'Mexican' },
+                                ]}
+                                className="mt-1 text-indigo-500 bg-indigo-500 mb-4"
+                                classNamePrefix="select"
+                                placeholder="Select Cuisines"
+                            />
+                        </label>
+                        <label className="block">
+                            Allergies:
+                            <Select
+                                isMulti
+                                name="allergies"
+                                options={[
+                                    { value: 'Peanuts', label: 'Peanuts' },
+                                    { value: 'Dairy', label: 'Dairy' },
+                                    { value: 'Gluten', label: 'Gluten' },
+                                    { value: 'Shellfish', label: 'Shellfish' },
+                                    { value: 'milk', label: 'milk' },
+                                    { value: 'NA', label: 'No allergies' }
+                                ]}
+                                className="mt-1 text-indigo-500 bg-indigo-500 mb-4"
+                                classNamePrefix="select"
+                                placeholder="Select Allergies"
 
+                            />
+                        </label>
+
+                        <label className="block">
+                            Go:
+                            <Select
+                                name="goal"
+                                options={[
+                                    { value: 'Weight loss', label: 'Weight Loss' },
+                                    { value: 'Weight Gain', label: 'Weight Gain' },
+                                    { value: 'Balanced Diet', label: 'Balanced Diet' },
+                                    { value: 'Boost Energy', label: 'Boost Energy' },
+                                    { value: 'No Goal Currently', label: 'No Goal Currently' }
+                                ]}
+                                className="mt-1 text-indigo-500 bg-indigo-500 mb-4"
+                                classNamePrefix="select"
+                                placeholder="Select Meals"
+                            />
+                        </label>
+                        <label className="block">
+                            Meals:
+                            <Select
+                                name="meals"
+                                options={[
+                                    { value: 'Breakfast', label: 'Breakfast' },
+                                    { value: 'Lunch', label: 'Lunch' },
+                                    { value: 'Dinner', label: 'Dinner' },
+                                    { value: 'Snacks', label: 'Snacks' },
+                                ]}
+                                className="mt-1 text-indigo-500 bg-indigo-500 mb-4"
+                                classNamePrefix="select"
+                                placeholder="Select Meals"
+                            />
+                        </label>
+                        <div className='mb-4'>
+                            <label htmlFor="mealsEveryday" className='block text-white'>How many meals per day? </label>
+                            <input type="text" id="mealsEveryday" className='border border-gray-300 p-2 rounded w-full' />
+                        </div>
+                        <div className='mb-4'>
+                            <label htmlFor="mealsTotal" className='block text-white'>How many days to plan for?</label>
+                            <input type="text" id="mealsTotal" className='border border-gray-300 p-2 rounded w-full' />
+                        </div>
+                       
                     </form>
                 </div>
+               
             </div>
-
+            <button className="block mx-auto bottom-4 right-4 bg-indigo-500 text-white px-4 py-2 rounded hover:bg-indigo-600">
+                     Submit 
+                </button>                         
         </div>
     )
 }
