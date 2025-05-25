ef743f7efa2fe66f75e96ec131aa2dafb4de284f
Koushik Roy <jc.koushikroy@gmail.com>
2025-05-25 15:45:16 +0530

    added CORS in controller

--- backend/src/main/java/com/nekocodes/freshly/controller/UserController.java
+++ backend/src/main/java/com/nekocodes/freshly/controller/UserController.java
@@ -6,6 +6,7 @@ import com.nekocodes.freshly.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class UserController {
 
