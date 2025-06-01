3f86d9c9258a09ba915152054e456091a13addff
Koushik Roy <jc.koushikroy@gmail.com>
2025-06-01 22:07:22 +0530

    fixed routers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider, Routes} from 'react-router-dom';
-import viteLogo from '/vite.svg'
-import './App.css'
-import InputForm from './components/InputForm'
-import Navbar from './components/Navbar'
-import SignUp from './components/SignUp'
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
-import HomePage from './pages/HomePage'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import InputForm from "./components/InputForm";
+import Navbar from "./components/Navbar";
+import SignUp from "./components/SignUp";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import HomePage from "./pages/HomePage";
 
 function App() {
-
-   return (
+  return (
     <Routes>
+      <Route path="/" element={<HomePage />}></Route>
       <Route path="/signup" element={<SignUp />} />
-      <Route path="/update/:email" element={<InputForm />} />
+      <Route path="/update" element={<InputForm />} />
     </Routes>
   );
 
@@ -25,12 +30,11 @@ function App() {
   //   <Route path='/' element={<HomePage/>}>
   //   <Route path='/signup' element= {<SignUp/>}/>
   //   <Route path='/update' element= {<InputForm/>}/>
-  //   </Route>  
-    
+  //   </Route>
+
   //   )
   // );
   // return <RouterProvider router={router} />;
-  
 }
 
-export default App
+export default App;
